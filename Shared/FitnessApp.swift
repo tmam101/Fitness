faa2cb700710821084ae79ad2c522bab599b2f48
Thomas Goss <thomas.goss@willowtreeapps.com>
2021-01-20 14:42:55 -0500

    Initial Commit

---
+++ Shared/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Shared
+//
+//  Created by Thomas Goss on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
