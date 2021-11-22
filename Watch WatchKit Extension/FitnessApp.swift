781c6d82a9da9b864629ce628ffa3ac8bffd2873
Thomas Goss <thomaspgoss98@gmail.com>
2021-11-21 19:02:45 -0500

    Create watch target

---
+++ Watch WatchKit Extension/FitnessApp.swift
@@ -0,0 +1,22 @@
+//
+//  FitnessApp.swift
+//  Watch WatchKit Extension
+//
+//  Created by Thomas Goss on 11/21/21.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    
+    @SceneBuilder var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+
+        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+    }
+}
