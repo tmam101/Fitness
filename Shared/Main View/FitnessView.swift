97c986395b9e227f9c60c9cb176265cdc50f177b
Thomas Goss <thomaspgoss98@gmail.com>
2022-03-29 13:51:41 -0400

    Add detail to weight loss graph

--- Shared/Main View/FitnessView.swift
+++ Shared/Main View/FitnessView.swift
@@ -74,6 +74,40 @@ struct FitnessView: View {
                                 await healthData.setValues(nil)
                             }
                         }
+                    Text("Expected Weight vs Weight Over Time")
+                        .foregroundColor(.white)
+                        .font(.title2)
+
+                    HStack {
+                        let expectedWeights = healthData.expectedWeights
+                        let weights = healthData.fitness.weights
+                        let dateToReach = Date.subtract(days: Int(deficitLineGraphDaysToShow), from: Date())
+                        let weightsFiltered = weights.filter { $0.date >= dateToReach }.map { $0.weight }
+                        let expectedWeightsFiltered = expectedWeights.filter { $0.date >= dateToReach }.map { $0.double }
+                        let expectedWeightChange = (expectedWeightsFiltered[expectedWeightsFiltered.count - 2]) - (expectedWeightsFiltered.first ?? 0)
+                        let weightChange = (weightsFiltered.first ?? 0) - (weightsFiltered.last ?? 0)
+                        let expectedWeightString = String(format: "%.2f", expectedWeightChange)
+                        let weightString = String(format: "%.2f", weightChange)
+
+                        VStack (alignment: .leading) {
+                            Text("Expected weight")
+                                .foregroundColor(.yellow)
+                            Text((expectedWeightChange >= 0 ? "+" : "") + "\(expectedWeightString)")
+                                .foregroundColor(.white)
+                        }
+                        .frame(maxWidth: .infinity)
+                        VStack (alignment: .leading) {
+                            Text("Weight")
+                                .foregroundColor(.green)
+                            Text((weightChange >= 0 ? "+" : "") + "\(weightString)")
+                                .foregroundColor(.white)
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .padding()
+                    .background(Color.myGray)
+                    .cornerRadius(20)
                     
                     ZStack {
                         DeficitAndWeightLossGraph(daysAgoToReach: $deficitLineGraphDaysToShow)
