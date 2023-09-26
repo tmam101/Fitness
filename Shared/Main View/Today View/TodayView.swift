685cfaf498b154ec27fc4eb4970a432986e7ee21
Thomas Goss <thomas.goss@willowtreeapps.com>
2023-09-26 15:04:38 -0400

    Refactor TodayView for clarity

--- Shared/Main View/Today View/TodayView.swift
+++ Shared/Main View/Today View/TodayView.swift
@@ -5,60 +5,27 @@
 //  Created by Thomas on 2/27/23.
 //
 
+// MARK: - Imports
+
 import SwiftUI
 import Charts
 import Combine
 
-// MARK: TODAY VIEW
+// MARK: - TodayView
+
 struct TodayView: View {
     @EnvironmentObject var vm: TodayViewModel
     @Environment(\.scenePhase) private var scenePhase
-    var environment: AppEnvironmentConfig = .release
-    let paddingAmount: CGFloat = 2 * 10
+    let paddingAmount: CGFloat = 20 // Instead of `2 * 10`, use a single value for clarity
     @State var columnCount: Int = 2
+    
+    // MARK: - Body
     
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
-                let today = vm.today
-                let overallItem = TodayRingViewModel(titleText: "Overall Score", bodyText: String(Int(vm.averagePercentage * 100)) + "%", subBodyText: "overall", percentage: vm.averagePercentage, bodyTextColor: .white, gradient: [.yellow, .purple, .orange, .yellow, .orange, .purple])
-                
-                let sign = today.surplus > 0 ? "+" : ""
-                let bodyText = "\(sign)\(Int(today.surplus))"
-                let color: TodayRingColor = today.surplus > 0 ? .red : .yellow
-                let netEnergyItem = TodayRingViewModel(titleText: "Net Energy", bodyText: bodyText, subBodyText: "cals", percentage: vm.deficitPercentage, color: .yellow, bodyTextColor: color, subBodyTextColor: color)
-                
-                let proteinItem = TodayRingViewModel(titleText: "Protein", bodyText: vm.proteinPercentage.percentageToWholeNumber() + "/30%", subBodyText: "cals", percentage: vm.proteinGoalPercentage, color: .purple, bodyTextColor: .purple, subBodyTextColor: .purple)
-                
-                let activeCalorieItem = TodayRingViewModel(titleText: "Active Calories", bodyText: String(Int(today.activeCalories)), subBodyText: "cals", percentage: vm.activeCaloriePercentage, color: .orange, bodyTextColor: .orange, subBodyTextColor: .orange)
-                
-                let weightChangeItem = TodayRingViewModel(titleText: "Weight Change", bodyText: today.expectedWeightChangedBasedOnDeficit.roundedString(), subBodyText: "pounds", percentage: vm.weightChangePercentage, color: .green, bodyTextColor: .green, subBodyTextColor: .green)
-                
-                let vms = [overallItem, proteinItem, activeCalorieItem, weightChangeItem, netEnergyItem]
-                
-                let columns: [GridItem] = {
-                    var c: [GridItem] = []
-                    for _ in 1...self.columnCount {
-                        c.append(GridItem(.adaptive(minimum: 400)))
-                    }
-                    return c
-                }()
-                Text("Fitness")
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .padding([.leading])
-                    .bold()
-                LazyVGrid(columns: columns, spacing: 20) {
-                    ForEach(vms, id: \.self) { item in
-                        TodayRingView(vm: item)
-                            .mainBackground()
-                    }
-                    TodayBar()
-                        .environmentObject(TodayBarViewModel(today: vm.today, maxValue: vm.maxValue, minValue: vm.minValue, yValues: vm.yValues))
-                        .padding()
-                        .mainBackground()
-                }
-                .padding(.horizontal)
+                displayFitnessHeader()
+                displayGridItems()
             }
         }
         .onAppear {
@@ -68,7 +35,99 @@ struct TodayView: View {
             vm.reloadToday()
         }
     }
+    
+    // MARK: - Private Helper Functions
+    
+    private func displayFitnessHeader() -> some View {
+        Text("Fitness")
+            .foregroundColor(.white)
+            .font(.title)
+            .padding([.leading])
+            .bold()
+    }
+    
+    private func displayGridItems() -> some View {
+        LazyVGrid(columns: createColumns(), spacing: 20) {
+            ForEach(createRingViewModels(), id: \.self) { item in
+                TodayRingView(vm: item)
+                    .mainBackground()
+            }
+            TodayBar()
+                .environmentObject(TodayBarViewModel(today: vm.today, maxValue: vm.maxValue, minValue: vm.minValue, yValues: vm.yValues))
+                .padding()
+                .mainBackground()
+        }
+        .padding(.horizontal)
+    }
+    
+    private func createColumns() -> [GridItem] {
+        Array(repeating: GridItem(.adaptive(minimum: 400)), count: columnCount)
+    }
+    
+    private func createRingViewModels() -> [TodayRingViewModel] {
+        // Construct and return an array of TodayRingViewModel objects
+        let today = vm.today
+
+        let overallItem = TodayRingViewModel(
+            titleText: "Overall Score",
+            bodyText: "\(Int(vm.averagePercentage * 100))%",
+            subBodyText: "overall",
+            percentage: vm.averagePercentage,
+            bodyTextColor: .white,
+            gradient: [.yellow, .purple, .orange, .yellow, .orange, .purple]
+        )
+        
+        let sign = today.surplus > 0 ? "+" : ""
+        let bodyText = "\(sign)\(Int(today.surplus))"
+        let color: TodayRingColor = today.surplus > 0 ? .red : .yellow
+        let netEnergyItem = TodayRingViewModel(
+            titleText: "Net Energy",
+            bodyText: bodyText,
+            subBodyText: "cals",
+            percentage: vm.deficitPercentage,
+            color: .yellow,
+            bodyTextColor: color,
+            subBodyTextColor: color
+        )
+        
+        let proteinItem = TodayRingViewModel(
+            titleText: "Protein",
+            bodyText: vm.proteinPercentage.percentageToWholeNumber() + "/30%",
+            subBodyText: "cals",
+            percentage: vm.proteinGoalPercentage,
+            color: .purple,
+            bodyTextColor: .purple,
+            subBodyTextColor: .purple
+        )
+        
+        let activeCalorieItem = TodayRingViewModel(
+            titleText: "Active Calories",
+            bodyText: "\(Int(today.activeCalories))",
+            subBodyText: "cals",
+            percentage: vm.activeCaloriePercentage,
+            color: .orange,
+            bodyTextColor: .orange,
+            subBodyTextColor: .orange
+        )
+        
+        let weightChangeItem = TodayRingViewModel(
+            titleText: "Weight Change",
+            bodyText: today.expectedWeightChangedBasedOnDeficit.roundedString(),
+            subBodyText: "pounds",
+            percentage: vm.weightChangePercentage,
+            color: .green,
+            bodyTextColor: .green,
+            subBodyTextColor: .green
+        )
+        
+        return [overallItem, proteinItem, activeCalorieItem, weightChangeItem, netEnergyItem]
+    }
+
 }
+
+// Rest of the code remains unchanged...
+
+
 
 struct TodayViewPreview: View {
     @State var vm = TodayViewModel(today: TestData.today, environment: .debug)
