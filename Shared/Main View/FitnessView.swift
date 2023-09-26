45546869d1be8adbb9d6022d27a87bc9e71c8e27
Thomas Goss <thomas.goss@willowtreeapps.com>
2023-09-26 16:48:56 -0400

    Refactor FitnessView

--- Shared/Main View/FitnessView.swift
+++ Shared/Main View/FitnessView.swift
@@ -9,126 +9,136 @@ import SwiftUI
 
 struct FitnessView: View {
     @EnvironmentObject var healthData: HealthData
-    //    @EnvironmentObject var watchConnectivityIphone: WatchConnectivityIphone
     @Environment(\.scenePhase) private var scenePhase
-    var shouldShowText: Bool = true
-    var lineWidth: CGFloat = 10
-    var widget: Bool = false
-    @State var isDisplayingOverlay = false
-    @State var deficitLineGraphDaysToShow: Double = 30.0
-    @State var runsToShow: Double = 5.0
-    @State var showLifts = false
-    @State var showWeightRings = false
-    @State var showWeeklyDeficitLine = false
     
+    // Variables
+    @State private var isDisplayingOverlay = false
+    @State private var deficitLineGraphDaysToShow: Double = 30.0
+    @State private var runsToShow: Double = 5.0
+    @State private var showLifts = false
+    @State private var showWeightRings = false
+    @State private var showWeeklyDeficitLine = false
+
+    // MARK: - Body
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
-                let sectionHeight: CGFloat = 400
-                
-                //MARK: DEFICIT RINGS
-                Group {
-                    HStack {
-                        StatsTitle(title: "Deficits")
-                        if !healthData.hasLoaded {
-                            Circle()
-                                .fill()
-                                .foregroundColor(.red)
-                                .frame(width: 20)
-                        }
-                    }
-                    StatsRow(text: { DeficitText() }, rings: { DeficitRings()})
-                        .environmentObject(healthData)
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                }
-                
-                // MARK: DEFICIT BAR CHART
-                Group {
-                    Text("Deficits This Week")
-                        .foregroundColor(.white)
-                        .font(.title2)
-                    SwiftUIBarChart(health: healthData)
-//                        .environmentObject(healthData)
-                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: sectionHeight)
-                        .mainBackground()
-//                    BarChart(showCalories: true)
-//                        .environmentObject(healthData)
-//                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: sectionHeight)
-//                        .mainBackground()
-//                        .animation(/*@START_MENU_TOKEN@*/.easeIn/*@END_MENU_TOKEN@*/, value: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
-                }
-                
-                //MARK: DEFICIT LINE GRAPH
-                if showWeeklyDeficitLine {
-                    Group {
-                        Text("Expected Weight This Week")
-                            .foregroundColor(.white)
-                            .font(.title2)
-                        DeficitLineGraph()
-                            .environmentObject(healthData)
-                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 200)
-                            .mainBackground()
-                    }
-                }
-                Group {
-                    if showWeightRings {
-                        //MARK: WEIGHT Loss
-                        StatsTitle(title: "Weight Loss")
-                        StatsRow(text: { WeightLossText() }, rings: { WeightLossRings() })
-                            .environmentObject(healthData)
-                            .frame(minWidth: 0, maxWidth: .infinity)
-                    }
-                    SwiftUILineChart(health: healthData)
-                        .mainBackground()
-                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 200)
+                // MARK: Deficit Rings
+                renderDeficitRingsSection()
 
+                // MARK: Deficit Bar Chart
+                renderDeficitBarChartSection()
+
+                // MARK: Deficit Line Graph
+                if showWeeklyDeficitLine {
+                    renderDeficitLineGraphSection()
                 }
-                
-                //MARK: MILE TIME
-                Group {
-                    StatsTitle(title: "Mile Time")
-                    MileTimeStats(runsToShow: $runsToShow)
-                        .environmentObject(healthData)
-                        .mainBackground()
-                        .frame(maxWidth: .infinity)
-                    RunningLineGraph(runsToShow: $runsToShow)
-                        .environmentObject(healthData)
-                        .frame(minWidth: 0, maxWidth: .infinity, idealHeight: sectionHeight)
-                        .padding()
-                        .mainBackground()
-                    if healthData.runManager.runs.count > 1 {
-                        Slider(
-                            value: $runsToShow,
-                            in: 1...Double(healthData.runManager.runs.count),
-                            step: 1 //todo this doesnt reach the first point. need to make sure it does
-                        )
-                            .tint(.blue)
-                        
-                        Text("past \(Int(runsToShow)) runs")
-                            .foregroundColor(.blue)
-                    }
-                }
+
+                renderWeightRingsAndLineChartSection()
+
+                // MARK: Mile Time
+                renderMileTimeSection()
             }
             .padding()
         }
         .onChange(of: scenePhase) { _ in
-            if scenePhase == .background {
-                Task {
-                    await healthData.setValues(completion: nil) //todo add force load here?
+            handleSceneChange()
+        }
+    }
+    
+    // MARK: - View Rendering Functions
+    
+    // TODO: Do I need ViewBuilder?
+    @ViewBuilder
+    private func renderDeficitRingsSection() -> some View {
+        Group {
+            HStack {
+                StatsTitle(title: "Deficits")
+                if !healthData.hasLoaded {
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 20)
                 }
+            }
+            StatsRow(text: { DeficitText() }, rings: { DeficitRings() })
+                .frame(maxWidth: .infinity)
+        }
+    }
+    
+    @ViewBuilder
+    private func renderDeficitBarChartSection() -> some View {
+        Group {
+            Text("Deficits This Week")
+                .foregroundColor(.white)
+                .font(.title2)
+            SwiftUIBarChart(health: healthData)
+                .frame(maxWidth: .infinity, minHeight: 400)
+                .mainBackground()
+        }
+    }
+    
+    @ViewBuilder
+    private func renderDeficitLineGraphSection() -> some View {
+        Group {
+            Text("Expected Weight This Week")
+                .foregroundColor(.white)
+                .font(.title2)
+            DeficitLineGraph()
+                .frame(maxWidth: .infinity, minHeight: 200)
+                .mainBackground()
+        }
+    }
+    
+    @ViewBuilder
+    private func renderWeightRingsAndLineChartSection() -> some View {
+        Group {
+            if showWeightRings {
+                StatsTitle(title: "Weight Loss")
+                StatsRow(text: { WeightLossText() }, rings: { WeightLossRings() })
+                    .frame(maxWidth: .infinity)
+            }
+            SwiftUILineChart(health: healthData)
+                .frame(maxWidth: .infinity, minHeight: 200)
+                .mainBackground()
+        }
+    }
+    
+    @ViewBuilder
+    private func renderMileTimeSection() -> some View {
+        Group {
+            StatsTitle(title: "Mile Time")
+            MileTimeStats(runsToShow: $runsToShow)
+                .frame(maxWidth: .infinity)
+                .mainBackground()
+            RunningLineGraph(runsToShow: $runsToShow)
+                .frame(maxWidth: .infinity, idealHeight: 400)
+                .padding()
+                .mainBackground()
+            if healthData.runManager.runs.count > 1 {
+                Slider(value: $runsToShow, in: 1...Double(healthData.runManager.runs.count), step: 1)
+                    .tint(.blue)
+                Text("past \(Int(runsToShow)) runs")
+                    .foregroundColor(.blue)
+            }
+        }
+    }
+    
+    // MARK: - Helper Functions
+    
+    private func handleSceneChange() {
+        if scenePhase == .background {
+            Task {
+                await healthData.setValues(completion: nil)
             }
         }
     }
 }
 
+
+
 struct FitnessView_Previews: PreviewProvider {
-    
     static var previews: some View {
         FitnessPreviewProvider.MainPreview()
-//        AppView()
-//            .environmentObject(HealthData(environment: .debug))
-//            .previewDevice(PreviewDevice(rawValue: "iPhone 13 Pro Max"))
-//        //            .environmentObject(WatchConnectivityIphone())
     }
 }
 
