36f9af79521cee292be8a4bf727abf44bb531f21
Thomas Goss <thomas.goss@willowtreeapps.com>
2023-11-16 14:40:14 -0500

    Add tests

--- Shared/Main View/FitnessApp.swift
+++ Shared/Main View/FitnessApp.swift
@@ -10,7 +10,7 @@ import WatchConnectivity
 
 @main
 struct FitnessApp: App {
-    @State var healthData = HealthData(environment: AppEnvironmentConfig.release )
+    @State var healthData = HealthData(environment: AppEnvironmentConfig.debug )
     @State var watchConnectivityIphone = WatchConnectivityIphone()
     @Environment(\.scenePhase) private var scenePhase
 
--- Shared/Main View/FitnessView.swift
+++ Shared/Main View/FitnessView.swift
@@ -16,6 +16,7 @@ struct TimeFramePicker: View {
             }
         }
         .pickerStyle(SegmentedPickerStyle())
+        .accessibilityIdentifier("Picker2")
     }
 }
 
