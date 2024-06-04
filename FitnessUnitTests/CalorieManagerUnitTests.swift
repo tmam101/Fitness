c164b175932e9f050acbafa91966faa86fd5f60f
Thomas Goss <thomas.goss@willowtreeapps.com>
2024-06-04 13:50:44 -0400

    Create CalorieManagerUnitTests file

---
+++ FitnessUnitTests/CalorieManagerUnitTests.swift
@@ -0,0 +1,16 @@
+//
+//  CalorieManagerUnitTests.swift
+//  FitnessUnitTests
+//
+//  Created by Thomas on 6/4/24.
+//
+
+import XCTest
+@testable import Fitness
+
+class CalorieManagerUnitTests: XCTestCase {
+    func testSomething() {
+        
+    }
+}
+
