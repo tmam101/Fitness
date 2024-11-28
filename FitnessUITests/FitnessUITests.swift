68edc677d1c141553ec0160be2055c62b8ab5815
Thomas Goss <thomaspgoss98@gmail.com>
2024-11-27 21:46:30 -0500

    Remove old code

--- FitnessUITests/FitnessUITests.swift
+++ FitnessUITests/FitnessUITests.swift
@@ -19,11 +19,6 @@ final class FitnessUITests: XCTestCase {
 
     override func setUpWithError() throws {
         try super.setUpWithError()
-//        let app = XCUIApplication()
-//        let x = Filepath.Days.firstDayNotAdjustingWhenMissing
-//        app.launchArguments = ["UITEST"]
-//        app.launch()
-//        self.app = app
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
